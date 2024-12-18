53144391fe01a11c8540a918424718e0a0dcc00c
Dina Laura <64177544+dinalaura@users.noreply.github.com>
2024-12-18 12:28:58 -0600

    Update KB.vue
    Signed-off-by: Dina Laura <64177544+dinalaura@users.noreply.github.com>

--- frontend/src/views/KB.vue
+++ frontend/src/views/KB.vue
@@ -6,7 +6,7 @@
     <div class="row">
       <div class="col-lg-6 mb-4" style="min-width: 750px;">
         <div class="card-header" style="background-color:#C398BD">
-          RAG - Knowledge Bases
+          Chatbot AnyCompany for Innovatech client
         </div>
         <div class="card">
           <img class="card-img-top" src="" alt=""/>
